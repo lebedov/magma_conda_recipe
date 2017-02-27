e05598c77a32f5a0273917c2d86ad75a3999189b
Lev E. Givon <lev@columbia.edu>
2017-02-26 21:48:29 -0500

    Update to 2.2.0.

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 export CUDADIR=/usr/local/cuda
-cp -f make.inc.openblas make.inc
+cp -f make.inc-examples/make.inc.openblas make.inc
 sed -i 's/^#GPU_TARGET ?= Fermi Kepler/GPU_TARGET ?= Fermi Kepler Maxwell/' make.inc
 sed -i 's/^CC\s*= gcc/CC = gcc-4.9/' make.inc
 sed -i 's/^CXX\s*= g++/CXX = g++-4.9/' make.inc
 sed -i 's/^FORT\s*= gfortran/FORT = gfortran-4.9/' make.inc
-
+sed -i 's/^NVCC\s*= nvcc/NVCC = \/usr\/local\/cuda\/bin\/nvcc/' make.inc
 cat >> make.inc <<EOF
 CFLAGS += -D_FORCE_INLINES
 CXXFLAGS += -D_FORCE_INLINES
 NVCCFLAGS += -D_FORCE_INLINES
-LIB += -lgfortran
 EOF
 
-make -j $CPU_COUNT
-make
+export OPENBLASDIR=$CONDA_PREFIX
+make -j $CPU_COUNT shared lib
 prefix=$PREFIX make install
