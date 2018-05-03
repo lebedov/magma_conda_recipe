2ca28cabbbc0c17666fe3f628ed602128e6ed149
Lev E. Givon <lev@columbia.edu>
2018-05-03 07:51:33 -0400

    Update to 2.3.0, use default system gcc, move CUDA version from package name to build string.

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 export CUDADIR=/usr/local/cuda
 cp -f make.inc-examples/make.inc.openblas make.inc
 sed -i 's/^#GPU_TARGET ?= Fermi Kepler/GPU_TARGET ?= Fermi Kepler Maxwell/' make.inc
-sed -i 's/^CC\s*= gcc/CC = gcc-4.9/' make.inc
-sed -i 's/^CXX\s*= g++/CXX = g++-4.9/' make.inc
-sed -i 's/^FORT\s*= gfortran/FORT = gfortran-4.9/' make.inc
 sed -i 's/^NVCC\s*= nvcc/NVCC = \/usr\/local\/cuda\/bin\/nvcc/' make.inc
 cat >> make.inc <<EOF
 CFLAGS += -D_FORCE_INLINES
