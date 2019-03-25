71493ca0094adfe1b47c7ee2bded5d94f8e60d57
Lev E. Givon <lev@columbia.edu>
2019-03-25 08:06:28 -0400

    Update to 2.5.0.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 export CUDADIR=/usr/local/cuda
 cp -f make.inc-examples/make.inc.openblas make.inc
-sed -i 's/^#GPU_TARGET ?= Fermi Kepler/GPU_TARGET ?= Fermi Kepler Maxwell/' make.inc
+sed -i 's/^#GPU_TARGET ?= Fermi Kepler/GPU_TARGET ?= Kepler Maxwell Pascal/' make.inc
 sed -i 's/^NVCC\s*= nvcc/NVCC = \/usr\/local\/cuda\/bin\/nvcc/' make.inc
 cat >> make.inc <<EOF
 CFLAGS += -D_FORCE_INLINES
