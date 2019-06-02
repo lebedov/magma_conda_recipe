2b1419d616f150f0d0c68afbf054bc67d738eb7f
Lev E. Givon <lev@columbia.edu>
2019-06-02 19:08:58 -0400

    Fix build, deps.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 export CUDADIR=/usr/local/cuda
 cp -f make.inc-examples/make.inc.openblas make.inc
-sed -i 's/^#GPU_TARGET ?= Fermi Kepler/GPU_TARGET ?= Kepler Maxwell Pascal/' make.inc
+sed -i 's/^#GPU_TARGET ?= \([[:alnum:][:blank:]]\+\)/GPU_TARGET ?= sm_35/' make.inc
 sed -i 's/^NVCC\s*= nvcc/NVCC = \/usr\/local\/cuda\/bin\/nvcc/' make.inc
+sed -i 's/^#OPENBLASDIR ?= \/usr\/local\/openblas/OPENBLASDIR ?= \($PREFIX\)/' make.inc
 cat >> make.inc <<EOF
 CFLAGS += -D_FORCE_INLINES
 CXXFLAGS += -D_FORCE_INLINES
